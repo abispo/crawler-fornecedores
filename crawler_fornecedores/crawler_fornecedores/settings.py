bc2941a36b3711e1516c544fc735c986cd82cb75
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2014-07-22 17:01:22 -0300

    Estrutura inicial do projeto scrapy

---
+++ crawler_fornecedores/crawler_fornecedores/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for crawler_fornecedores project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'crawler_fornecedores'
+
+SPIDER_MODULES = ['crawler_fornecedores.spiders']
+NEWSPIDER_MODULE = 'crawler_fornecedores.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'crawler_fornecedores (+http://www.yourdomain.com)'
