bf77d37f820ef14ed0c94e8702b7084d67c4638b
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2014-07-23 13:07:31 -0300

    Coleta das informações sobre o item

--- crawler_fornecedores/crawler_fornecedores/spiders/feiradamadrugada_carteiras.py
+++ crawler_fornecedores/crawler_fornecedores/spiders/feiradamadrugada_carteiras.py
@@ -1,4 +1,6 @@
 import scrapy
+import re
+
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors import LinkExtractor
 from crawler_fornecedores.items import FeiraDaMadrugadaCarteiras
@@ -21,5 +23,23 @@ class FeiraDaMadrugadaCarteirasSpider(CrawlSpider):
 		
 		item['url'] = response.url.split("?")[0]
 		refCode = response.selector.xpath("//div[@class='elementos']/h1/text()").extract()[0].split(" ")[-1:]
-		print refCode[0]
+		item['refCode'] = refCode[0].replace("#", "")
 
+		item['price'] = response.selector.xpath("//div[@class='elementos']/div[@class='valores']/text()").extract()[0].strip()
+		item['discountPrice'] = response.selector.xpath("//div[@class='elementos']/div[@class='valores']/span/span[@class='productSpecialPrice']/text()").extract()[0].strip()
+		inStock = response.selector.xpath("//div[@class='marg_top10']/span/text()").extract()[0]
+		item['inStock'] = re.match(r"[a-zA-Z-#]*[0-9-]*", inStock).group()
+		item['description'] = response.selector.xpath("string(//div[@class='desc3']/p[1])").extract()[0].strip()
+		item['dimensions'] = response.selector.xpath("//div[@class='desc3']/p[3]/text()").extract()[0].strip()
+
+		model = re.match(r"[a-zA-Z-#]*[0-9-]*", item['refCode']).group()
+
+		print item['url']
+		print item['refCode'].upper()
+		print model
+		print item['price']
+		print item['discountPrice']
+		print item['inStock']
+		print item['description']
+		print item['dimensions']
+		print "=" * 100
